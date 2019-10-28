b094f0cf0ac653b0271b78522dc6907fcea63ab2
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2019-10-28 22:14:25 +0100

    Implement prototype for TreeMap

---
+++ treemap.py
@@ -0,0 +1,28 @@
+from treeset import TreeSet
+
+
+class TreeMap(dict):
+    def __init__(self, seq=None, **kwargs):
+        if seq is None:
+            super().__init__(**kwargs)
+        else:
+            super().__init__(seq, **kwargs)
+        self.sorted_keys = TreeSet(super().keys())
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.sorted_keys.add(key)
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        self.sorted_keys.remove(key)
+
+    def keys(self):
+        return self.sorted_keys
+
+    def items(self):
+        return [(k, self[k]) for k in self.sorted_keys]
+
+    def __iter__(self):
+        for k in self.sorted_keys:
+            yield k
